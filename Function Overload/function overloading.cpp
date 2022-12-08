b464f54e5408161eda6287bedae05570292855f6
Shubham Giri <99181962+ShubhamGiri26@users.noreply.github.com>
2022-12-08 21:44:07 +0530

    Create function overloading.cpp

---
+++ Function Overload/function overloading.cpp
@@ -0,0 +1,23 @@
+#include<iostream.h>
+#include<stdio.h>
+int sum(int x, int y)
+float sum(float x, float y)
+int sum(int x, int y,int z)
+void main()
+{
+cout<<"Sum="<<Sum(2,3)<<endl;
+cout<<"Sum="<<Sum(1.5,1.5)<<endl;
+cout<<"Sum="<<Sum(1.2,3)<<endl;
+}
+int sum(int x,int y)
+{
+return x+y+z;
+}
+float sum(float x, float y)
+{
+return x+y;
+}
+int sum(int x,int y,int z)
+{
+return x+y+z;
+}
