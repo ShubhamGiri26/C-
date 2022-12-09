b000c3482eb5524dcd5a71012a372fbc42d7f28d
Shubham Giri <99181962+ShubhamGiri26@users.noreply.github.com>
2022-12-09 09:40:23 +0530

    Create Variables of templates.cpp

---
+++ Templates/Variables of templates.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+template<class T>
+class x
+{
+
+    public:
+    X(T,i)
+    {
+        cout<<"\n size"<<sizeof(i);
+    }
+}
+
+void main()
+{
+    X<int> o1<2>;
+    X<char>o2<'x'>;
+}
